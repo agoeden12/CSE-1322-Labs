7609d36e688231ced96ac2d7d8aca8c8b73a8f13
Andrew Goeden <andrewgoeden@gmail.com>
2019-10-19 19:02:26 -0400

    Format Document

--- Labs/agoeden/Recursion.java
+++ Labs/agoeden/Recursion.java
@@ -3,16 +3,16 @@ package agoeden;
 public class Recursion {
 
 	public static boolean palindromeCheck(String input) {
-    char[] letters = input.toCharArray();
-    if (letters.length < 2) {
-      return true;
-    } else if (letters[0] == letters[letters.length - 1]){
-      return palindromeCheck(input.substring(1, input.length() - 1));
-    }
-    return false;
-  }
+		char[] letters = input.toCharArray();
+		if (letters.length < 2) {
+			return true;
+		} else if (letters[0] == letters[letters.length - 1]) {
+			return palindromeCheck(input.substring(1, input.length() - 1));
+		}
+		return false;
+	}
 
-  public static void main(String[] args) {
-    System.out.println(palindromeCheck("mam"));
-  }
+	public static void main(String[] args) {
+		System.out.println(palindromeCheck("mam"));
+	}
 }
